4d4cb93b8f897412b72f7fe5ddf6cb5abaf7d727
ricardo <ramn21@gmail.com>
2020-10-18 03:02:34 +0200

    test scraper

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -2,13 +2,21 @@ require_relative '../lib/scraper'
 
 describe Scraper do
   let(:url){'https://www.minecraftglobal.com/minecraft-news/'}
-  let(:a) {Scraper.new(url).content}
+  let(:scrap) {Scraper.new(url)}
   describe "#content" do
     it 'return an array' do
-      expect(a).to be_kind_of Array
+      expect(scrap.content).to be_kind_of Array
     end
     it 'will contain info' do
-      expect(a).not_to be_empty
+      expect(scrap.content).not_to be_empty
+    end
+  end
+  describe "#complete_content" do
+    it 'return an array' do
+      expect(scrap.complete_content).to be_kind_of Array
+    end
+    it 'will contain info' do
+      expect(scrap.complete_content).not_to be_empty
     end
   end
 end
