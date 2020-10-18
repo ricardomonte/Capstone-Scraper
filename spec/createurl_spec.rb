a5429d3b79eeeb0b25e29ac807e28b3e25af1f6c
ricardo <ramn21@gmail.com>
2020-10-18 03:34:51 +0200

    improve createurl_spec

--- spec/createurl_spec.rb
+++ spec/createurl_spec.rb
@@ -5,14 +5,14 @@ describe CreateUrl do
   let(:create) { CreateUrl.new }
   describe '#create_url' do
     it 'return a url in a specific position' do
-      dbl = double('some scraper', minecraft: [[1, ['a', 'b', 'c', 'd', 'https://www.minecraftglobal.com']]])
+      dbl = double('some scraper', minecraft: [[1, ['a', 'b', 'c', 'd', url]]])
       mine = dbl.minecraft
-      expect(create.create_url(1, mine)).to eq('https://www.minecraftglobal.com')
+      expect(create.create_url(1, mine)).to eq(url)
     end
     it 'will not return an element that is not in a specific position' do
-      dbl = double('some scraper', minecraft: [[1, ['a', 'https://www.minecraftglobal.com', 'c', 'd', 'hello world']]])
+      dbl = double('some scraper', minecraft: [[1, ['a', url, 'c', 'd', 'hello world']]])
       mine = dbl.minecraft
-      expect(create.create_url(1, mine)).not_to eq('https://www.minecraftglobal.com')
+      expect(create.create_url(1, mine)).not_to eq(url)
     end
   end
 end
